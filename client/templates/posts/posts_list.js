0ed6ac8e4fd83c0f564a82d047ed4eee9ca7c8ba
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-10-24 13:35:14 -0500

    publications

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,22 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  },
-  {
-    title: 'Woah, Dude',
-    url: 'http://old.reddit.com/r/woahdude'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function () {
+    return Posts.find();
+  }
 });
