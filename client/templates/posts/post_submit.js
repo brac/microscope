5796fd2831e6d389e8a3372d3a2cd390c0b14ea3
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-10-25 12:44:20 -0500

    domain bug

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -2,15 +2,18 @@ Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
 
+
     var post = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
 
     Meteor.call('postInsert', post, function(error, result) {
-      if (error) return alert(error.reason)
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
 
-      Router.go('postPage', {_id: result._id})
-    })
+      Router.go('postPage', {_id: result._id});
+    });
   }
-});
+})
