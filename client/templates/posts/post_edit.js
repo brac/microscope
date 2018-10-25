8b1a9857ff84b429446fb3c8de7d5faf0330a879
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-10-25 14:54:09 -0500

    permissions and editing posts

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -5,8 +5,8 @@ Template.postEdit.events({
     var currentPostId = this._id
 
     var postProperties = {
-      url: $(e.target).find('name=url').val(),
-      title: $(e.target).find('name=title').val()
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
     }
 
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
